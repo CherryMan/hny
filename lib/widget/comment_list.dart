11211fe62068f5513866ee7378a14eacbd3e9091
Sheheryar Parvaz <skipper308@hotmail.ca>
2018-09-08 12:04:40 -0400

    Fix comments unloading when outside of viewport

--- lib/widget/comment_list.dart
+++ lib/widget/comment_list.dart
@@ -10,41 +10,19 @@ class CommentList extends StatelessWidget {
   final List<int> _commentIDs;
   final int _depth;
 
-  Widget _buildCommentThread(Comment c) {
-
-    var comm = CommentTile(c);
-
-    if (c.kids != null) {
-      var subComments =
-        Padding(
-            padding: const EdgeInsets.only(left: 15.0),
-            child: CommentList(c.kids, depth: _depth + 1),
-        );
-
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          comm,
-          subComments,
-        ]
-      );
-    }
-
-    return comm;
-  }
-
   @override
   Widget build(BuildContext _) => ListView.builder(
     physics: const ClampingScrollPhysics(),
     shrinkWrap: true,
     itemCount: _commentIDs.length,
-    itemBuilder: (_, i) => FutureBuilder<Comment>(
-      future: Comment.fromID(_commentIDs[i]),
-      builder: (_, snap) {
-        if      (snap.hasData)  return _buildCommentThread(snap.data);
-        else if (snap.hasError) return Text('${snap.error}');
-        else return Container(child: CircularProgressIndicator());
-      },
-    ),
+
+    itemBuilder: (ctx, i) {
+
+      // Prevent the comment from being unloaded
+      KeepAliveNotification(KeepAliveHandle())
+        .dispatch(ctx);
+
+      return CommentTile(_commentIDs[i], depth: _depth);
+    },
   );
 }
--- lib/widget/comment_tile.dart
+++ lib/widget/comment_tile.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../hn/data.dart';
+import 'comment_list.dart';
 
 class CommentTile extends StatelessWidget {
-  CommentTile(this._comment);
+  CommentTile(this._commentID, {int depth: 0})
+    : _depth = depth;
 
-  final Comment _comment;
+  final int _depth;
+  final int _commentID;
+
+  Comment _comment;
 
   final _infoStyle = const
     TextStyle(
@@ -18,27 +23,78 @@ class CommentTile extends StatelessWidget {
       fontWeight: FontWeight.normal,
     );
 
-  @override
-  Widget build(BuildContext _) {
+  Widget _buildTile() {
+
+    String author = '${_comment?.by}'   ?? '...';
+    String text   = '${_comment?.text}' ?? '...';
+
+    final col =
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top:2.0, bottom: 6.0),
+            child: Text(author,   style: _infoStyle),
+          ),
+          Text(text, style: _textStyle),
+        ],
+      );
+
     return Card(
       child: Container(
         margin: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(top:2.0, bottom: 6.0),
-              child: Text('${_comment.by}',   style: _infoStyle),
-            ),
-            Row(
-              children: [
-                Expanded(child: Text('${_comment.text}', style: _textStyle)),
-              ],
-            ),
-          ],
-        ),
+        child: col,
       ),
     );
+  }
+
+  Widget _buildSubComments(List<int> kids) =>
+    Padding(
+      padding: const EdgeInsets.only(left: 15.0),
+      child: CommentList(kids, depth: _depth + 1),
+    );
+
+  Widget _buildThread(Comment c) {
+
+    var comm =
+      Row(
+        children: [ Expanded(child: _buildTile()) ],
+      );
+
+    if (c?.kids != null) {
+      var subComments = _buildSubComments(c.kids);
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          comm,
+          subComments,
+        ]
+      );
+    }
+
+    return comm;
+  }
+
+  @override
+  Widget build(BuildContext _) {
+
+    var fb =
+      FutureBuilder<Comment>(
+        future: Comment.fromID(_commentID),
+        builder: (_, snap) {
+          if (snap.hasData) {
+            _comment = snap.data;
+            return _buildThread(snap.data);
+          } else if (snap.hasError) {
+            return Text('${snap.error}');
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      );
+
+    return fb;
   }
 }
